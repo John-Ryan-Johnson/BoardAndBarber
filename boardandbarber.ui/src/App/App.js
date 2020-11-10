f34b18462ea5043a0a8b1ce6861dfe5a780940d4
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-11-10 16:34:27 -0600

    authentication works with proper api key

--- boardandbarber.ui/src/App/App.js
+++ boardandbarber.ui/src/App/App.js
@@ -1,10 +1,20 @@
 import './App.scss';
+import fbConnection from "../helpers/data/connection";
 import Customers from '../components/pages/Customers/Customers';
+import Login from '../components/pages/Login/Login';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+fbConnection();
 
 function App() {
   return (
-    <div className='App'>
-      <Customers />
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/Login" component={Login}></Route>
+          <Route path="/Customers" component={Customers}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
